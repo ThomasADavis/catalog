075aebfa5f39f0e86f693a5894142210dd4e7ef1
Joshua Boverhof <boverhof@186.215.128.198.in-addr.dhcp.lbnl.us>
2016-06-02 11:36:02 -0700

    On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   dockerfiles/modbus/setup.sh     -- update image to 0.6 version

--- dockerfiles/modbus/setup.sh
+++ dockerfiles/modbus/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="mini/modbus-worker:0.5"
+DOCKER_IMAGE_NAME="mini/modbus-worker:0.6"
 
 main() {
     if [ ! -d pulsar ] ; then
-        git clone https://boverhof@bitbucket.org/crtsensors/pulsar.git
+        git clone https://boverhof@bitbucket.org/boverhof/pulsar.git
     fi
     if [ ! -d pulsar-docker ] ; then
         git clone https://boverhof@bitbucket.org/crtsensors/pulsar-docker.git
