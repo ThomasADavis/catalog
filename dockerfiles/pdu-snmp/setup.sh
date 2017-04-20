a9e24adacc5eeb804cafe33f9cf7d221a98d10a0
Thomas Davis <tdavis@imap.nersc.gov>
2017-04-19 20:47:10 -0700

    Fix apc param problem.

--- dockerfiles/pdu-snmp/setup.sh
+++ dockerfiles/pdu-snmp/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/snimpy:0.14"
+DOCKER_IMAGE_NAME="alpine/snimpy:0.15"
 
 main() {
     if [ ! -d pulsar ] ; then
