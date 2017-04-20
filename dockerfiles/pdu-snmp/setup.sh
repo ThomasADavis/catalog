46a0c564bd3adee83b1c4e2ad0a78924daf2147d
Thomas Davis <tdavis@imap.nersc.gov>
2017-04-19 20:25:54 -0700

    Updates to v0.3

--- dockerfiles/pdu-snmp/setup.sh
+++ dockerfiles/pdu-snmp/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/snimpy:0.12"
+DOCKER_IMAGE_NAME="alpine/snimpy:0.13"
 
 main() {
     if [ ! -d pulsar ] ; then
