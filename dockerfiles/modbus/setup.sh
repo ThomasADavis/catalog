5ba1c0df8678bf09f5cd7a5194e8e4af6f79e75d
Josh Robert Boverhof <jrboverhof@lbl.gov>
2017-04-27 10:59:59 -0700

    modified:   dockerfiles/modbus/Dockerfile 	modified:   dockerfiles/modbus/setup.sh -- after building image create container and run unittest before pushing to repo

--- dockerfiles/modbus/setup.sh
+++ dockerfiles/modbus/setup.sh
@@ -2,13 +2,16 @@
 set -e
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="mini/modbus-worker:0.15"
+DOCKER_IMAGE_NAME="mini/modbus-worker:0.16"
 
 main() {
     if [ ! -d pulsar ] ; then
         #git clone git@bitbucket.org:crtsensors/pulsar.git
         git clone https://bitbucket.org/crtsensors/pulsar.git
+    else 
+        ( cd pulsar && git pull )
     fi
+
     if [ ! -d pulsar-docker ] ; then
         #git clone git@bitbucket.org:crtsensors/pulsar-docker.git
         git clone https://bitbucket.org/crtsensors/pulsar-docker.git
@@ -19,9 +22,13 @@ main() {
         chmod 755 get-pip.py
     fi
 
-    docker build -t ${DOCKER_IMAGE_NAME} . \
-    && docker tag $DOCKER_IMAGE_NAME $REGISTRY_NAME:$REGISTRY_PORT/$DOCKER_IMAGE_NAME \
-    && docker push $REGISTRY_NAME:$REGISTRY_PORT/$DOCKER_IMAGE_NAME
+    docker build -t ${DOCKER_IMAGE_NAME} . 
+    echo ">>> RUN TEST"
+    docker run --rm --name=pulsar_test1 -w /application/pulsar/test --entrypoint=python -ti mini/modbus-worker:0.16 -m unittest crt_modbus_mock_test
+
+    echo ">>> TAG AND PUSH"
+    docker tag $DOCKER_IMAGE_NAME $REGISTRY_NAME:$REGISTRY_PORT/$DOCKER_IMAGE_NAME \
+    docker push $REGISTRY_NAME:$REGISTRY_PORT/$DOCKER_IMAGE_NAME
 }
 
 main
