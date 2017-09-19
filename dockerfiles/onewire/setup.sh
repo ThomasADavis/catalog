7b706334027b93d492a181b4171fbece4280a758
Thomas Davis <tdavis@imap.nersc.gov>
2017-09-19 10:51:59 -0700

    Fix breakout of linke scanner.

--- dockerfiles/onewire/setup.sh
+++ dockerfiles/onewire/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/owfs:0.9"
+DOCKER_IMAGE_NAME="alpine/owfs:0.10"
 
 main() {
     if [ ! -d pulsar ] ; then
