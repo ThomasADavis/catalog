d7760683d7f6e6e6d7be5a896e81f806ce3d120f
Thomas Davis <tdavis@imap.nersc.gov>
2017-10-24 13:11:20 -0700

    Add ceiling support.

--- dockerfiles/onewire/setup.sh
+++ dockerfiles/onewire/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/owfs:14"
+DOCKER_IMAGE_NAME="alpine/owfs:16"
 
 main() {
     if [ ! -d pulsar ] ; then
