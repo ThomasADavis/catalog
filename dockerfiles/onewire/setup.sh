5bb75a19a00ebe7450eadae2273a10cc8a5048b1
Thomas Davis <tdavis@imap.nersc.gov>
2017-10-24 13:52:42 -0700

    More fixes.

--- dockerfiles/onewire/setup.sh
+++ dockerfiles/onewire/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/owfs:17"
+DOCKER_IMAGE_NAME="alpine/owfs:18"
 
 main() {
     if [ ! -d pulsar ] ; then
