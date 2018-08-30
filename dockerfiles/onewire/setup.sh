5cab91c55da62d4f18a612f4f86db98e76c95ca7
Thomas Davis <tdavis@imap.nersc.gov>
2018-08-30 13:34:35 -0700

    added new gateway c1-onewire-gw7

--- dockerfiles/onewire/setup.sh
+++ dockerfiles/onewire/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/owfs:21"
+DOCKER_IMAGE_NAME="alpine/owfs:22"
 
 main() {
     if [ ! -d pulsar ] ; then
         git clone git@bitbucket.org:crtsensors/pulsar.git
     fi
-    if [ ! -d pulsar-docker ] ; then
-       git clone git@bitbucket.org:crtsensors/pulsar-docker.git
-    fi
-
+#    if [ ! -d pulsar-docker ] ; then
+#       git clone git@bitbucket.org:crtsensors/pulsar-docker.git
+#    fi
+#
 #    if [ ! -x "get-pip.py" ] ; then
 #        curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 #        chmod 755 get-pip.py
