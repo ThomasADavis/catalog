5b129bf3190c7a23d3ec4fddcadc8f6350432e58
root <root@docker-dev.crt.nersc.gov>
2018-12-13 12:03:10 -0800

    version bump

--- dockerfiles/weewx/setup.sh
+++ dockerfiles/weewx/setup.sh
@@ -2,7 +2,7 @@
 
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="centos/weewx:0.7"
+DOCKER_IMAGE_NAME="centos/weewx:0.8"
 
 main() {
 #    if [ ! -d pulsar ] ; then
