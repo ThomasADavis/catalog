e4008bb6858f1b2536e7fdcf3f716c6df62ec717
root <root@docker-dev.crt.nersc.gov>
2019-01-17 16:09:10 -0800

    added new config for metric

--- dockerfiles/weewx/setup.sh
+++ dockerfiles/weewx/setup.sh
@@ -2,7 +2,7 @@
 
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="centos/weewx:0.9"
+DOCKER_IMAGE_NAME="centos/weewx:0.91"
 
 main() {
 #    if [ ! -d pulsar ] ; then
