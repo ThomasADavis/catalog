1a7c44ec83ff2848d69340431c19f66e4f984a6b
root <root@docker-dev.crt.nersc.gov>
2019-03-18 23:07:38 -0700

    update onewire stack eeprom scan

--- dockerfiles/onewire/setup.sh
+++ dockerfiles/onewire/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_NAME='docker-registry.crt.nersc.gov'
 REGISTRY_PORT=5000
-DOCKER_IMAGE_NAME="alpine/owfs:23"
+DOCKER_IMAGE_NAME="alpine/owfs:27"
 
 main() {
     if [ ! -d pulsar ] ; then
